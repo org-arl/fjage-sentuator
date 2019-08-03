4243ed0970a8081eca8cc7f7446877dfc1611f0a
Mandar Chitre <mandar@arl.nus.edu.sg>
2019-08-04 01:48:54 +0800

    test(sentuator): add regression tests

---
+++ src/test/groovy/org/arl/fjage/sentuator/SentuatorSpec.groovy
@@ -0,0 +1,248 @@
+import spock.lang.*
+import org.arl.fjage.*
+import org.arl.fjage.remote.Gateway
+import org.arl.fjage.sentuator.*
+
+class SentuatorSpec extends Specification {
+
+  static class MySentuator extends Sentuator {
+
+    static class MyMeasurement extends Measurement {
+      String type
+      float x
+    }
+
+    int setupCount = 0
+    int setupCountAtStartup = 0
+    float x = 0
+
+    @Override
+    void setup() {
+      config.ofs = 0.0
+      setupCount++
+    }
+
+    @Override
+    void startup() {
+      setupCountAtStartup = setupCount
+    }
+
+    @Override
+    Measurement measure() {
+      return new MyMeasurement(x: x+config.ofs)
+    }
+
+    @Override
+    Measurement measure(String type) {
+      return new MyMeasurement(type: type, x: x+1)
+    }
+
+    @Override
+    boolean actuate(Object value) {
+      x = value as float
+      return true
+    }
+
+    @Override
+    boolean actuate(String type, Object value) {
+      if (type == 'bad') {
+        if (value) setStatus(Status.WARNING, 'Bad actuation')
+        else setStatus(Status.OK)
+        return true
+      }
+      return false
+    }
+
+  }
+
+  @Shared Platform platform
+  @Shared Container container
+  @Shared MySentuator aut
+  @Shared Gateway gw
+
+  def setupSpec() {
+    GroovyExtensions.enable()
+    platform = new RealTimePlatform()
+    container = new Container(platform)
+    aut = new MySentuator()
+    container.add 'aut', aut
+    platform.start()
+    gw = new Gateway(container)
+  }
+
+  def cleanupSpec() {
+    platform.shutdown()
+  }
+
+  def "setup, then startup" () {
+    expect:
+      aut.setupCount == 1
+      aut.setupCountAtStartup == 1
+  }
+
+  def "must advertise service" () {
+    expect:
+      gw.agentForService(org.arl.fjage.sentuator.Services.SENTUATOR)?.name == aut.name
+  }
+
+  def "status OK" () {
+    when:
+      def aid = gw.agentForService(org.arl.fjage.sentuator.Services.SENTUATOR)
+      def rsp = gw.request new StatusReq(recipient: aid)
+    then:
+      rsp.status == Status.OK
+      rsp.time >= 0
+  }
+
+  def "basic actuation and measurement" () {
+    when:
+      def aid = gw.agentForService(org.arl.fjage.sentuator.Services.SENTUATOR)
+      def rsp1 = aid << new ActuationReq(1.0)
+      def rsp2 = aid << new MeasurementReq()
+      def rsp3 = aid << new ActuationReq(2.0)
+      def rsp4 = aid << new MeasurementReq()
+    then:
+      rsp1.performative == Performative.AGREE
+      rsp2 instanceof Measurement
+      rsp2.performative == Performative.INFORM
+      rsp2.x == 1.0
+      rsp2.time >= 0
+      rsp3.performative == Performative.AGREE
+      rsp4 instanceof Measurement
+      rsp4.performative == Performative.INFORM
+      rsp4.x == 2.0
+      rsp4.time >= rsp2.time
+  }
+
+  def "typed actuation and measurement" () {
+    when:
+      def aid = gw.agentForService(org.arl.fjage.sentuator.Services.SENTUATOR)
+      def rsp1 = aid << new ActuationReq(7.0)
+      def rsp2 = aid << new MeasurementReq('special')
+      def rsp3 = aid << new ActuationReq('special', 2.0)
+      def rsp4 = aid << new MeasurementReq('special')
+    then:
+      rsp1.performative == Performative.AGREE
+      rsp2 instanceof Measurement
+      rsp2.performative == Performative.INFORM
+      rsp2.type == 'special'
+      rsp2.x == 8.0
+      rsp2.time >= 0
+      rsp3.performative == Performative.REFUSE
+      rsp4 instanceof Measurement
+      rsp4.performative == Performative.INFORM
+      rsp4.type == 'special'
+      rsp4.x == 8.0
+      rsp4.time >= rsp2.time
+  }
+
+  def "disable and enable" () {
+    when:
+      def aid = gw.agentForService(org.arl.fjage.sentuator.Services.SENTUATOR)
+      aid << new ConfigurationReq().set('enable', false)
+      def rsp1 = aid << new StatusReq()
+      def rsp2 = aid << new MeasurementReq()
+      def rsp3 = aid << new ActuationReq(0)
+      aid << new ConfigurationReq().set('enable', true)
+      def rsp4 = aid << new StatusReq()
+      def rsp5 = aid << new MeasurementReq()
+      def rsp6 = aid << new ActuationReq(0)
+    then:
+      rsp1.status == Status.DISABLED
+      rsp1.time >= 0
+      rsp2.performative == Performative.REFUSE
+      rsp3.performative == Performative.REFUSE
+      rsp4.status == Status.OK
+      rsp4.time >= 0
+      rsp5.performative == Performative.INFORM
+      rsp6.performative == Performative.AGREE
+  }
+
+  def "get configuration" () {
+    when:
+      def aid = gw.agentForService(org.arl.fjage.sentuator.Services.SENTUATOR)
+      def rsp1 = aid << new ConfigurationReq().get('ofs')
+      def rsp2 = aid << new ConfigurationReq().get('missing')
+      def rsp3 = aid << new ConfigurationReq().get('missing').get('ofs')
+      def rsp4 = aid << new ConfigurationReq()
+    then:
+      rsp1.keys().size() == 1
+      rsp1.get('ofs') == 0.0
+      rsp2.performative == Performative.REFUSE
+      rsp3.keys().size() == 1
+      rsp3.get('ofs') == 0.0
+      rsp4.keys().size() == 3
+      rsp4.get('ofs') == 0.0
+      rsp4.get('enable') == true
+      rsp4.get('poll') == 0
+  }
+
+  def "set configuration" () {
+    when:
+      def aid = gw.agentForService(org.arl.fjage.sentuator.Services.SENTUATOR)
+      aid << new ActuationReq(1.0)
+      def rsp1 = aid << new ConfigurationReq().set('ofs', 1.0)
+      def rsp2 = aid << new MeasurementReq()
+      def rsp3 = aid << new ConfigurationReq().get('ofs')
+      def rsp4 = aid << new ConfigurationReq().set('ofs', 0.0)
+      def rsp5 = aid << new MeasurementReq()
+      def rsp6 = aid << new ConfigurationReq().set('bad', 1.0)
+      def rsp7 = aid << new ConfigurationReq().get('bad')
+    then:
+      rsp1.keys().size() == 1
+      rsp1.get('ofs') == 1.0
+      rsp2.x == 2.0
+      rsp3.get('ofs') == 1.0
+      rsp4.keys().size() == 1
+      rsp4.get('ofs') == 0.0
+      rsp5.x == 1.0
+      rsp6.performative == Performative.REFUSE
+      rsp7.performative == Performative.REFUSE
+  }
+
+  def "status management" () {
+    when:
+      def aid = gw.agentForService(org.arl.fjage.sentuator.Services.SENTUATOR)
+      def rsp1 = aid << new StatusReq()
+      def rsp2 = aid << new ActuationReq('bad', true)
+      def rsp3 = aid << new StatusReq()
+      def rsp4 = aid << new ActuationReq('bad', false)
+      def rsp5 = aid << new StatusReq()
+    then:
+      rsp1.status == Status.OK
+      rsp1.message == null
+      rsp2.performative == Performative.AGREE
+      rsp3.status == Status.WARNING
+      rsp3.message == 'Bad actuation'
+      rsp4.performative == Performative.AGREE
+      rsp5.status == Status.OK
+      rsp5.message == null
+  }
+
+  def "polling" () {
+    when:
+      def aid = gw.agentForService(org.arl.fjage.sentuator.Services.SENTUATOR)
+      gw.subscribe(gw.topic(aid))
+      aid << new ActuationReq(1.0)
+      aid << new ConfigurationReq().set("poll", 100)
+      sleep(1000)
+      def ntf
+      int cnt1 = 0
+      while ((ntf = gw.receive()) != null) {
+        if (ntf instanceof Measurement) cnt1++
+      }
+      aid << new ConfigurationReq().set("poll", 0)
+      sleep(1000)
+      int cnt2 = 0
+      while ((ntf = gw.receive()) != null) {
+        if (ntf instanceof Measurement) cnt2++
+      }
+      gw.unsubscribe(gw.topic(aid))
+    then:
+      println("expecting 10 messages, got ${cnt1}")
+      cnt1 >= 9
+      cnt1 <= 11
+      cnt2 == 0
+  }
+
+}
